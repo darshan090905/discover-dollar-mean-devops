4cbe2b552f832554a712fe8db478cf8585005657
Ubuntu <ubuntu@ip-172-31-24-201.ec2.internal>
2026-02-24 08:10:00 +0000

    Fixed merge conflict in db.config.js

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
-  url: "mongodb://localhost:27017/testdb"
-=======
   url: "mongodb://mongodb:27017/testdb"
->>>>>>> 45d83f4 (Added GitHub Actions CI/CD pipeline)
 };
